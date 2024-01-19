eb9224fcebc9385e4be82601109bf666f028b265
Shivam mahendru <147685720+shhhivam12@users.noreply.github.com>
2024-01-20 02:24:23 +0530

    Delete temp.java

--- temp.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-public class temp {
-    public static void main(String[] args) {
-            String todaydate= ""+LocalDate.now();
-            System.out.println(todaydate);
-    }
-}
